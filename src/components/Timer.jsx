eae715e12fbd673e1fe9c5e33f331681b8367c9d
hongZhou443 <67214990+hongZhou443@users.noreply.github.com>
2024-05-05 19:10:08 -0500

    set timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import './Timer.css';
 
 const Timer = () => {
-    const [time, setTime] = useState("00:00:00");
-    const Ref = useRef(null);
+    const [time, setTime] = useState('00:00:00');
+    const [timerMinute, setTimerMinute] = useState(0);
+    const [timerSecond, setTimerSecond] = useState(10);
+    const [timerMinuteSet, setTimerMinuteSet] = useState(timerMinute);
+    const [timerSecondSet, setTimerSecondSet] = useState(timerSecond);
+    const ref = useRef(null);
 
     const remainingTime = (e) => {
-        // time from current time to future time
         const totalTime = Date.parse(e) - Date.parse(new Date());
         const hours = Math.floor((totalTime / 1000 / 60 / 60) % 24);
         const minutes = Math.floor((totalTime / 1000 / 60) % 60);
@@ -16,58 +19,101 @@ const Timer = () => {
             hours,
             minutes,
             seconds,
-        }
-    }
+        };
+    };
 
     const start = (e) => {
-        let {totalTime, hours, minutes, seconds} = remainingTime(e);
+        const { totalTime, hours, minutes, seconds } = remainingTime(e);
         if (totalTime >= 0) {
+            let displayTime = formatTime(hours, minutes, seconds);
+            setTime(displayTime);
+        }
+    };
+
+    const formatTime = (hours, minutes, seconds) => {
+        if (hours + minutes + seconds >= 0) {
             // add leading 0s if needed
             if (hours <= 9) {
-                hours = "0" + hours;
+                hours = '0' + hours;
             }
             if (minutes <= 9) {
-                minutes = "0" + minutes;
+                minutes = '0' + minutes;
             }
             if (seconds <= 9) {
-                seconds = "0" + seconds;
+                seconds = '0' + seconds;
             }
-            setTime(hours + ":" + minutes + ":" + seconds);
         }
-    }
+
+        return hours + ':' + minutes + ':' + seconds;
+    };
 
     const clear = (e) => {
-        // 15 minute switchoff during swarms
-        setTime("00:15:00");
+        let displayTime = formatTime(0, timerMinute, timerSecond);
+        setTime(displayTime);
 
-        if (Ref.current) {
-            clearInterval(Ref.current);
+        if (ref.current) {
+            clearInterval(ref.current);
         }
         const id = setInterval(() => {
             start(e);
         }, 1000);
-        Ref.current = id;
-    }
+        ref.current = id;
+    };
 
     const setDeadline = () => {
-        // if need more time in current session, add 2 minutes
         let deadline = new Date();
-        deadline.setMinutes(deadline.getMinutes() + 15);
+        deadline.setMinutes(deadline.getMinutes() + timerMinute);
+        deadline.setSeconds(deadline.getSeconds() + timerSecond);
         return deadline;
-    }
+    };
 
-    useEffect(() => {
-        clear(setDeadline());
-    }, []);
+    // useEffect(() => {
+    //     clear(setDeadline());
+    // }, [timerMinute, timerSecond]);
 
     const reset = () => {
         clear(setDeadline());
+    };
+
+    const setTimer = () => {
+        setTimerMinute(timerMinuteSet);
+        setTimerSecond(timerSecondSet);
     }
 
     return (
-        <div className="timer">
+        <div className="timer-wrapper">
+            <div className="set-timer">
+                {formatTime(0, timerMinute, timerSecond)}
+            </div>
             {time}
-            <button onClick={reset}>reset</button>
+            <form className="set-timer-wrapper">
+                <label>
+                    Set Minute
+                    <input
+                        name="setMinute"
+                        type="number"
+                        min="0"
+                        max="59"
+                        onChange={(e) => {
+                            setTimerMinuteSet(parseInt(e.target.value, 10));
+                        }}
+                    />
+                </label>
+                <label>
+                    Set Second
+                    <input
+                        name="setSecond"
+                        type="number"
+                        min="0"
+                        max="59"
+                        onChange={(e) => {
+                            setTimerSecondSet(parseInt(e.target.value, 10));
+                        }}
+                    />
+                </label>
+            </form>
+            <button onClick={setTimer}>Set Time</button>
+            <button onClick={reset}>Start</button>
         </div>
     );
 };
