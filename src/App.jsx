208b8a301cc41291af4a24678f7ef82090e6245d
Joel Kim <joelkim2026@u.northwestern.edu>
2024-05-13 02:23:05 -0500

    firebase host setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import Timer from './components/Timer';
 
+
+// firebase import
+import { initializeApp } from "firebase/app";
+
+// firebase connfitg
+const firebaseConfig = {
+  apiKey: "AIzaSyAlAO_z2B82y8MWjngetQDtk1HU5EC0Xf4",
+  authDomain: "yourturn-orange.firebaseapp.com",
+  projectId: "yourturn-orange",
+  storageBucket: "yourturn-orange.appspot.com",
+  messagingSenderId: "519165106567",
+  appId: "1:519165106567:web:ca2edcd2f679a0ee275b22",
+  measurementId: "G-KTW54R6PYP"
+};
+
+// firebase app
+const app = initializeApp(firebaseConfig);
+
 const App = () => {
   const [count, setCount] = useState(0);
 
